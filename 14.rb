02f20867a5b284a725170b57061f20a5b8468965
Anatoly <39996329+Grap-Anatoly@users.noreply.github.com>
2019-09-11 15:48:15 +0300

    Add files via upload

---
+++ 14.rb
@@ -0,0 +1,14 @@
+r=5;
+m1=15;
+m2=25;
+G=6.738;
+
+F=G*((m1*m2)/r**2);
+
+print"Sila prityazheniya ";
+puts(F);
+
+
+
+
+
---
+++ 15.rb
@@ -0,0 +1,11 @@
+g=25;
+k1=5;
+
+k2=Math.sqrt(g**2-k1**2);
+r=(1/2)*(k1+k2-g);
+
+print "Katet ";
+puts(k2);
+
+print "Radius ";
+puts(r);
